efc8fc1250dc9c207e6d935b668daea6c06e0c57
ddwugu <136143092+ddwugu@users.noreply.github.com>
2024-03-08 22:03:43 +0700

    Stream_data.py

--- Stream_data.py
+++ Stream_data.py
@@ -11,10 +11,12 @@ except Exception as e:
 
 # Web Title
 st.title('Pertamina Field Jambi')
-st.subheader('Prediksi Lokasi Kebocoran Line BJG-TPN')
+
 # User Inputs
 Titik_1_PSI = st.text_input('Input Pressure di titik 1 (PSI)')
 Titik_2_PSI = st.text_input('Input Pressure di titik 2 (PSI)')
+a = 135 - float(Titik_1_PSI) if Titik_1_PSI else None
+b = 86 - float(Titik_2_PSI) if Titik_2_PSI else None
 
 # Code prediction
 suspect_loct = ''
@@ -22,13 +24,16 @@ suspect_loct = ''
 # Prediction Button
 if LokasiKM is not None and st.button('Prediksi Lokasi'):
     try:
-        prediksi_lokasi = LokasiKM.predict([[float(Titik_1_PSI), float(Titik_2_PSI)]])
-        if prediksi_lokasi[0] == 0: #titik nol
-            suspect_loct = 'It is safe that there is no fluid flowing'
-        elif prediksi_lokasi[0] >= 26.2: #total panjang trunkline
-            suspect_loct = 'Safe, there are no leaks'
+        if a is not None and b is not None:
+            prediksi_lokasi = LokasiKM.predict([[a, b]])
+            if prediksi_lokasi[0] == 0: # titik nol
+                suspect_loct = 'It is safe that there is no fluid flowing'
+            elif prediksi_lokasi[0] >= 26.3: # total panjang trunkline
+                suspect_loct = 'Safe, there are no leaks'
+            else:
+                suspect_loct = f'!!!estimated leak location {prediksi_lokasi[0]} KM'
+            st.success(suspect_loct)
         else:
-            suspect_loct = f'!!!estimated leak location {prediksi_lokasi[0]} KM'
-        st.success(suspect_loct)
+            st.warning("Masukkan tekanan yang valid untuk kedua titik.")
     except Exception as e:
         st.error(f"Error predicting location: {e}")
